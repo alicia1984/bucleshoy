eeb610094e340156f08096e7ccdd9bf9460e2a41
alicia1984 <alicia28031@hotmail.com>
2017-11-03 21:52:38 -0300

    trabajando con bucles

---
+++ src/bucles/Bucles.java
@@ -0,0 +1,25 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bucles;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author LP 13
+ */
+public class Bucles {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    int stop = -1;
+    while (stop<=0)//mientras  se cumpla  esta  condicion
+    {
+stop = Integer.parseInt(JOptionpane.showInputDialog("ingrese un  numero"))
+}
+}
